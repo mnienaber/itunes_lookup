11d1a6e9edab3ba7832b3b901f590ad9cc24f3da
Michael Nienaber <mnienaber@mnienaber-macbookpro.roam.corp.google.com>
2016-08-17 22:44:21 +1000

    live search working
    just not populating the tableview

--- itunes_lookup/ViewController.swift
+++ itunes_lookup/ViewController.swift
@@ -12,30 +12,83 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
 
     var searchResultsDict = [SearchResultsDict]()
 
-    @IBOutlet weak var textFieldOutlet: UITextField!
     @IBOutlet weak var searchBarOutlet: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
 
+    var searchActive: Bool = false
+    var data = [String: AnyObject]()
+    var filtered = [SearchResultsDict]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchBarOutlet.showsScopeBar = true
+        searchBarOutlet.delegate = self
+        tableView.delegate = self
+    }
+
+
+//    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+//        searchActive = true;
+//    }
+//
+//    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+//        searchActive = false;
+//    }
+//
+//    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+//        searchActive = false;
+//    }
+//
+//    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+//        searchActive = false;
+//    }
+
+    func searchBar(searchBar: UISearchBar, textDidChange query: String) {
+
+
+        print(query)
+
+
+        performUIUpdatesOnMain {
+
+            self.testApi(query)
+        }
+
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
-    @IBAction func goButton(sender: AnyObject) {
+        let cellReuseIdentifier = "TableViewCell"
+        let searchResult = Client.sharedInstance().searchResults[indexPath.row]
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier) as UITableViewCell!
+
+        cell.textLabel!.text = searchResult.artistName
+        cell.detailTextLabel!.text = searchResult.bundleId
+        print(searchResult.bundleId)
+
+        return cell
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return Client.sharedInstance().searchResults.count
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+
+        let location = Client.sharedInstance().searchResults[indexPath.row]
+        let url = location.bundleId
         
-        let query = searchBarOutlet.text
-        testApi(query!)
         
     }
 
-    
+
+
     func testApi(query: String) {
 
         Client.sharedInstance().getSearchItems(query) { (searchResultsDict, error) in
@@ -51,15 +104,13 @@ class ViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate
 
                 performUIUpdatesOnMain {
 
-                    dispatch_async(dispatch_get_main_queue(), {
-                        let controller = self.storyboard!.instantiateViewControllerWithIdentifier("TableViewControllerForSegue")
-                        self.presentViewController(controller, animated: true, completion: nil)
-                    })
+
+                    self.tableView.reloadData()
                 }
-
-
             }
         }
-    }
 }
+}
+
+
 
