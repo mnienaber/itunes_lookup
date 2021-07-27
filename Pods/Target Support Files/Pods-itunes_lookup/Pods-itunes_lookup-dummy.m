c63fcd54d4564b4193896ce9a88717a1ceec7981
Michael Nienaber <mpn@protonmail.com>
2021-07-26 21:11:23 -0400

    removal Pods folder

--- Pods/Target Support Files/Pods-itunes_lookup/Pods-itunes_lookup-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_itunes_lookup : NSObject
-@end
-@implementation PodsDummy_Pods_itunes_lookup
-@end
--- Pods/Target Support Files/Pods-itunes_lookupUITests/Pods-itunes_lookupUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_itunes_lookupUITests : NSObject
-@end
-@implementation PodsDummy_Pods_itunes_lookupUITests
-@end
